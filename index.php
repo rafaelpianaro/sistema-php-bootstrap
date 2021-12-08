b9d490b61fd018869c923bc3061ab8c165fd4dee
Rafael <rafaelpianaro@gmail.com>
2021-12-08 16:03:09 -0300

    *modal delete *icon

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include_once('conection.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="/images/logo.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <!-- <link href="/css/bootstrap.min.css" rel="stylesheet"> -->
     <title>CRUD - PHP FETCH</title>
@@ -112,6 +113,29 @@ include_once('conection.php');
                 </div>
             </div>
         </div>
+    </div>
+    <!-- MODAL DELETE -->
+    <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="deleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h5 class="modal-title" id="deleteModalLabel">Deletar usuário</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <dl class="row">
+                <dt class="col-sm-3">Nome</dt>
+                <dd class="col-sm-9"><span id="nomeDelete"></span></dd>
+                <dt class="col-sm-3">E-mail</dt>
+                <dd class="col-sm-9"><span id="mailDelete"></span></dd>
+            </dl>
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Fechar</button>
+            <button type="button" class="btn btn-outline-primary btn-sm" id="confirmDelete">Confirmar</button>
+        </div>
+        </div>
+    </div>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <!-- <script src="js/bootstrap.bundle.min.js"></script> -->
