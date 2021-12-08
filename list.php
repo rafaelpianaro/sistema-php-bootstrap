df22e7186962ee8bf76a85a90d78fa95e40942cc
Rafael <rafaelpianaro@gmail.com>
2021-12-08 16:04:01 -0300

    *change button delete

--- list.php
+++ list.php
@@ -35,7 +35,9 @@ while($row_user = $result_users->fetch(PDO::FETCH_ASSOC)){
         <td>
             <button id='$id' class='btn btn-outline-primary btn-sm' onClick='viewUser($id)'>Visualizar</button>
             <button id='$id' class='btn btn-outline-warning btn-sm' onClick='editUser($id)'>Editar</button>
-            <button id='$id' class='btn btn-outline-danger btn-sm' onClick='deleteUser($id)'>Deletar</button>
+            <button id='$id' class='btn btn-outline-danger btn-sm' 
+            onClick='deleteUser($id)'
+            data-bs-toggle='modal' data-bs-target='#deleteModal'>Deletar</button>
         </td>
     </tr>";
 }
